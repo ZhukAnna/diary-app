4c2912b5141c906848d6c456ac17321d2b51d0fc
Anna Zhukova <anna.zhukanna@gmail.com>
2022-01-10 19:56:38 +0500

    fixed some build issues

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Diary App
 
 The application allows you to manage your tasks, save memories, ideas, deeds and share your thoughts with your friends.
+
+Demo page: https://zhukanna.github.io/diary-app/
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './nullstyle.css';
 import './index.css';
 import App from './components/App/App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
