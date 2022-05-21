09cbd7d0eab743c959953bd9e69eedfd8dfc904e
Anna Zhukova <anna.zhukanna@gmail.com>
2022-05-21 18:14:56 +0500

    refactor exchange component from class to functional

--- src/components/Exchange/Exchange.jsx
+++ src/components/Exchange/Exchange.jsx
@@ -1,59 +1,40 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Exchange.css';
 
 const API_KEY = process.env.REACT_APP_EXCHANGE_API_KEY;
 
-export default class Exchange extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            EUR: 0,
-            CAD: 0,
-            RUB: 0,
-            AUD: 0
-        };
-    }
+export default function Exchange() {
+  const [currency, setCurrency] = useState({ EUR: 0, CAD: 0, RUB: 0, AUD: 0 });
 
-    componentDidMount() {
-        this.getRates();
-    }
-
-    getRates = async () => {
-        fetch(`https://openexchangerates.org/api/latest.json?app_id=${API_KEY}`)
-        .then(response => response.json())
-        .then(data => {
-                this.setState({
-                    EUR: data.rates.EUR,
-                    CAD: data.rates.CAD,
-                    RUB: data.rates.RUB,
-                    AUD: data.rates.AUD,
-                });
-            })
-        .catch(error => {
-            console.log('Error fetching and parsing data', error);
+  useEffect(() => {
+    async function fetchData() {
+      await fetch(
+        `https://openexchangerates.org/api/latest.json?app_id=${API_KEY}&symbols=GBP,EUR,RUB,CAD`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setCurrency(data.rates);
+        })
+        .catch((error) => {
+          console.log('Error fetching and parsing data', error);
         });
-    }  
-
-    render({EUR, CAD, RUB, AUD} = this.state) {
-        return (
-            <div className="exchange">
-                <div className="exchange_item">
-                    <span className="exchange__currency">EUR</span>
-                    <span className="exchange__rate">{EUR}</span>
-                </div>
-                <div className="exchange_item">
-                    <span className="exchange__currency">RUB</span>
-                    <span className="exchange__rate">{RUB}</span>
-                </div>
-                <div className="exchange_item">
-                    <span className="exchange__currency">CAD</span>
-                    <span className="exchange__rate">{CAD}</span>
-                </div>
-                <div className="exchange_item">
-                    <span className="exchange__currency">AUD</span>
-                    <span className="exchange__rate">{AUD}</span>
-                </div>
-            </div>
-        )
     }
+    fetchData();
+  }, []);
+
+  let keys = Object.keys(currency);
+
+  return (
+    <div className="exchange">
+      {keys.map((key) => {
+        return (
+          <div className="exchange__item" key={key}>
+            <span className="exchange__currency">{key}</span>
+            <span className="exchange__rate">{currency[key].toFixed(2)}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
